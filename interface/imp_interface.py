1b40653c62d99e32c24dd1e2eb0dace296af73b2
Claire Deng <ClaireDeng@fico.com>
2019-01-22 11:13:45 -0800

    170 implement interfaces

---
+++ interface/imp_interface.py
@@ -0,0 +1,25 @@
+import math
+
+
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        divisors = set()
+        root = math.floor(math.sqrt(n))
+        for i in range(1, root + 1):
+            if n % i == 0:
+                divisors.add(i)
+                divisors.add(n // i)
+        # print(divisors)
+        return sum(divisors)
+
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
