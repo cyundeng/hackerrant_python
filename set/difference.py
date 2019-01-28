2ec6e1271d4a237b1ce46878285b52c36b68f792
Claire Deng <ClaireDeng@fico.com>
2019-01-27 20:51:56 -0800

    220 Difference from one set to another

---
+++ set/difference.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a = int(input())
+    set_a = set(map(int, input().split()))
+    b = int(input())
+    set_b = set(map(int, input().split()))
+    set_a = set_a - set_b
+    # print(set_a)
+    print(len(set_a))
